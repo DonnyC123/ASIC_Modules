679922c37d62257aeb7385d650ae5d5416eb1502
Donny <donnyc12@outlook.com>
2026-02-17 23:40:39 -0600

    Subtract Change

--- mac/float_design/mac_float.sv
+++ mac/float_design/mac_float.sv
@@ -153,7 +153,7 @@ module mac_float #(
   );
 
   always_comb begin
-    mantissa_sum_lower = csa_tree_sum + {csa_tree_carry[PRODUCT_MANTISSA_W-1:0], subtract_c};
+    mantissa_sum_lower = csa_tree_sum + {csa_tree_carry[PRODUCT_MANTISSA_W-1:1], subtract_c};
     mantissa_sum_upper = MANTISSA_SUM_HIGH_W'(c_shifted_eff[C_SHIFTED_W-1 : PRODUCT_MANTISSA_W]) 
                         + MANTISSA_SUM_HIGH_W'(mantissa_sum_lower[MANTISSA_SUM_LOW_W-1]);
 
