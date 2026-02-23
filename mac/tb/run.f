1eab72065204399f23f9c2cad9762a81409ee832
Donny <donnyc12@outlook.com>
2026-02-22 23:56:17 -0600

    changes

--- mac/tb/run.f
+++ mac/tb/run.f
@@ -4,6 +4,7 @@
 ../float_design/wallace_tree_recursive.sv
 ../float_design/special_float_handler.sv
 ../float_design/align_addend.sv
+../float_design/mac_float_decode.sv
 ../float_design/mac_float.sv
 
 ../tb/sanity_test.sv
