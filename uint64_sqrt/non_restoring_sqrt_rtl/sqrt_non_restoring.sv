cad20886ac98ed42fb307e821d8acad0a953bb1f
Donny <donnyc12@outlook.com>
2026-03-16 16:48:09 -0500

    non_restoring finished

--- uint64_sqrt/non_restoring_sqrt_rtl/sqrt_non_restoring.sv
+++ uint64_sqrt/non_restoring_sqrt_rtl/sqrt_non_restoring.sv
@@ -18,15 +18,18 @@ module sqrt_non_restoring #(
   localparam STAGE_STEPS           = DOUT_W / PIPELINE_STAGES;
   localparam REMAINING_STAGE_STEPS = DOUT_W - PIPELINE_STAGES * STAGE_STEPS;
 
-  logic [REMAINDER_W-1:0] AX     [PIPELINE_STAGES+1];
-  logic [ TEST_SUB_W-1:0] T      [PIPELINE_STAGES+1];
-  logic [     DOUT_W-1:0] Q      [PIPELINE_STAGES+1];
+  logic                   final_rem_is_neg;
+  logic [     DOUT_W-1:0] floored_root;
 
-  logic [REMAINDER_W-1:0] AX_next[PIPELINE_STAGES+1];
-  logic [ TEST_SUB_W-1:0] T_next [PIPELINE_STAGES+1];
-  logic [     DOUT_W-1:0] Q_next [PIPELINE_STAGES+1];
+  logic [REMAINDER_W-1:0] AX               [PIPELINE_STAGES+1];
+  logic [ TEST_SUB_W-1:0] T                [PIPELINE_STAGES+1];
+  logic [     DOUT_W-1:0] Q                [PIPELINE_STAGES+1];
 
-  logic                   valid  [PIPELINE_STAGES+1];
+  logic [REMAINDER_W-1:0] AX_next          [PIPELINE_STAGES+1];
+  logic [ TEST_SUB_W-1:0] T_next           [PIPELINE_STAGES+1];
+  logic [     DOUT_W-1:0] Q_next           [PIPELINE_STAGES+1];
+
+  logic                   valid            [PIPELINE_STAGES+1];
 
   always_comb begin
     AX[0][DIN_W-1:0]           = rad_i;
@@ -39,7 +42,7 @@ module sqrt_non_restoring #(
 
   genvar stage_idx;
   generate
-    for (stage_idx = 0; stage_idx < PIPELINE_STAGES; stage_idx++) begin : gen_pipeline
+    for (stage_idx = 0; stage_idx < PIPELINE_STAGES; stage_idx++) begin
 
       localparam SQRT_STEPS = (stage_idx == PIPELINE_STAGES -1) ?
                               REMAINING_STAGE_STEPS + STAGE_STEPS
@@ -57,14 +60,6 @@ module sqrt_non_restoring #(
           .Q_o (Q_next[stage_idx])
       );
 
-      logic [DOUT_W-1:0] Q_to_reg;
-
-      if (stage_idx == PIPELINE_STAGES - 1) begin : gen_final_correction
-        assign Q_to_reg = AX_next[stage_idx][REMAINDER_W-1] ? (Q_next[stage_idx] - 1'b1) : Q_next[stage_idx];
-      end else begin : gen_pass_through
-        assign Q_to_reg = Q_next[stage_idx];
-      end
-
       data_status_pipeline #(
           .DATA_W    (REMAINDER_W + TEST_SUB_W + DOUT_W),
           .STATUS_W  (1),
@@ -73,7 +68,7 @@ module sqrt_non_restoring #(
       ) data_status_pipeline_inst (
           .clk     (clk),
           .rst_n   (rst_n),
-          .data_i  ({AX_next[stage_idx], T_next[stage_idx], Q_to_reg}),
+          .data_i  ({AX_next[stage_idx], T_next[stage_idx], Q_next[stage_idx]}),
           .status_i(valid[stage_idx]),
           .data_o  ({AX[stage_idx+1], T[stage_idx+1], Q[stage_idx+1]}),
           .status_o(valid[stage_idx+1])
