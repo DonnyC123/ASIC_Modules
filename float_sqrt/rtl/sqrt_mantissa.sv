52d237a9a3418363d3c602fe4bc4a42948d62fb8
Donny <donnyc12@outlook.com>
2026-03-17 08:20:23 -0500

    bug fix in sqrt float

--- float_sqrt/rtl/sqrt_mantissa.sv
+++ float_sqrt/rtl/sqrt_mantissa.sv
@@ -15,11 +15,11 @@ module sqrt_mantissa #(
   import sqrt_float_pkg::*;
 
   localparam SIGN_W      = 1;
-  localparam REMAINDER_W = 2 * MANTISSA_W + SIGN_W;
-  localparam TEST_SUB_W  = MANTISSA_W + SIGN_W;
+  localparam REMAINDER_W = 2 * ROOT_EXTENDED_W + SIGN_W;
+  localparam TEST_SUB_W  = ROOT_EXTENDED_W + SIGN_W;
 
-  localparam STAGE_STEPS           = MANTISSA_W / PIPELINE_STAGES;
-  localparam REMAINING_STAGE_STEPS = MANTISSA_W - PIPELINE_STAGES * STAGE_STEPS;
+  localparam STAGE_STEPS           = ROOT_EXTENDED_W / PIPELINE_STAGES;
+  localparam REMAINING_STAGE_STEPS = ROOT_EXTENDED_W - PIPELINE_STAGES * STAGE_STEPS;
 
   logic                       final_rem_is_neg;
   logic [ROOT_EXTENDED_W-1:0] root_extended;
@@ -27,20 +27,20 @@ module sqrt_mantissa #(
 
   logic [    REMAINDER_W-1:0] AX               [PIPELINE_STAGES+1];
   logic [     TEST_SUB_W-1:0] T                [PIPELINE_STAGES+1];
-  logic [     MANTISSA_W-1:0] Q                [PIPELINE_STAGES+1];
+  logic [ROOT_EXTENDED_W-1:0] Q                [PIPELINE_STAGES+1];
 
   logic [    REMAINDER_W-1:0] AX_next          [PIPELINE_STAGES+1];
   logic [     TEST_SUB_W-1:0] T_next           [PIPELINE_STAGES+1];
-  logic [     MANTISSA_W-1:0] Q_next           [PIPELINE_STAGES+1];
+  logic [ROOT_EXTENDED_W-1:0] Q_next           [PIPELINE_STAGES+1];
   logic                       valid            [PIPELINE_STAGES+1];
 
   always_comb begin
-    AX[0]                           = {1'b0, mantissa_rad_i, {MANTISSA_W{1'b0}}};
-    valid[0]                        = valid_i;
+    AX[0][ROOT_EXTENDED_W-1:0]           = {1'b0, mantissa_rad_i};
+    valid[0]                             = valid_i;
 
-    AX[0][REMAINDER_W-1:MANTISSA_W] = '0;
-    T[0]                            = '0;
-    Q[0]                            = '0;
+    AX[0][REMAINDER_W-1:ROOT_EXTENDED_W] = '0;
+    T[0]                                 = '0;
+    Q[0]                                 = '0;
   end
 
   genvar stage_idx;
@@ -52,8 +52,8 @@ module sqrt_mantissa #(
                           : STAGE_STEPS;
 
       sqrt_non_restoring_stage #(
-          .DIN_W     (MANTISSA_W),
-          .DOUT_W    (MANTISSA_W),
+          .DIN_W     (ROOT_EXTENDED_W),
+          .DOUT_W    (ROOT_EXTENDED_W),
           .SQRT_STEPS(SQRT_STEPS)
       ) sqrt_non_restoring_stage_inst (
           .AX_i(AX[stage_idx]),
