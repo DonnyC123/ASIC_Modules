fff2f9fbf4014cbcc02a5bc0febe8f3d3a1ca130
Donny <donnyc12@outlook.com>
2026-03-19 18:50:35 -0500

    fixed clk enable signal

--- float_sqrt/tb/run.f
+++ float_sqrt/tb/run.f
@@ -9,13 +9,13 @@
 ../../common/rtl/leading_zero_counter.sv     
 ../../common/rtl/leading_zero_counter_top.sv 
         
-../srt_sqrt_rtl/srt_sqrt_pkg.sv             
-../srt_sqrt_rtl/srt_radix4_qds.sv           
-../srt_sqrt_rtl/srt_radix4_seed.sv          
-../srt_sqrt_rtl/carry_save_row_adder.sv     
-../srt_sqrt_rtl/leading_zero_counter.sv     
-../srt_sqrt_rtl/leading_zero_counter_top.sv 
-../srt_sqrt_rtl/srt_sqrt_stage.sv           
+../../uint64_sqrt/srt_sqrt_rtl/srt_sqrt_pkg.sv             
+../../uint64_sqrt/srt_sqrt_rtl/srt_radix4_qds.sv           
+../../uint64_sqrt/srt_sqrt_rtl/srt_radix4_seed.sv          
+../../uint64_sqrt/srt_sqrt_rtl/carry_save_row_adder.sv     
+../../uint64_sqrt/srt_sqrt_rtl/leading_zero_counter.sv     
+../../uint64_sqrt/srt_sqrt_rtl/leading_zero_counter_top.sv 
+../../uint64_sqrt/srt_sqrt_rtl/srt_sqrt_stage.sv           
 
 ../../uint64_sqrt/restoring_sqrt_rtl/sqrt_restoring_stage.sv
 
