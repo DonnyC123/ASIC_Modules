e272ad45c9e50153a0f45a74d94c795a8ee49140
Donny <donnyc12@outlook.com>
2026-03-19 18:49:32 -0500

    fixed clk enable signal

--- float_sqrt/tb/run.f
+++ float_sqrt/tb/run.f
@@ -8,11 +8,21 @@
                                              
 ../../common/rtl/leading_zero_counter.sv     
 ../../common/rtl/leading_zero_counter_top.sv 
-         
+        
+../srt_sqrt_rtl/srt_sqrt_pkg.sv             
+../srt_sqrt_rtl/srt_radix4_qds.sv           
+../srt_sqrt_rtl/srt_radix4_seed.sv          
+../srt_sqrt_rtl/carry_save_row_adder.sv     
+../srt_sqrt_rtl/leading_zero_counter.sv     
+../srt_sqrt_rtl/leading_zero_counter_top.sv 
+../srt_sqrt_rtl/srt_sqrt_stage.sv           
+
 ../../uint64_sqrt/restoring_sqrt_rtl/sqrt_restoring_stage.sv
+
 ../rtl/sqrt_float_pkg.sv                  
 ../rtl/sqrt_float_decoder.sv              
 ../rtl/sqrt_mantissa.sv
+../rtl/sqrt_srt_mantissa.sv
 ../rtl/root_rounder.sv
 ../rtl/sqrt_float.sv
 
