e72a0f528517ee3bfd9b1d408d2baf80cb757599
Donny <donnyc12@outlook.com>
2026-03-23 19:07:20 -0500

    smaller csa tree

--- float_mac/rtl/mac_float_execution.sv
+++ float_mac/rtl/mac_float_execution.sv
@@ -16,24 +16,51 @@ module mac_float_execution
     output logic [  FULL_SUM_CARRY_W-1:0] mantissa_sum_raw_o
 );
 
-  logic [PRODUCT_MANTISSA_W-1:0] mult_result;
-  logic [ PARTIAL_SUM_LOW_W-1:0] mantissa_sum_lower;
+  localparam NUM_PARTIAL_PRODUCTS = MANTISSA_W;
+  localparam NUM_WALLACE_INPUTS   = NUM_PARTIAL_PRODUCTS + 1;
+
+
+
+  logic [         LOW_SUM_W-1:0] partial_products   [NUM_PARTIAL_PRODUCTS];
+  logic [         LOW_SUM_W-1:0] csa_summands       [  NUM_WALLACE_INPUTS];
+  logic [         LOW_SUM_W-1:0] csa_tree_sum;
+  logic [         LOW_SUM_W-1:0] csa_tree_carry;
   logic [  PARTIAL_SUM_HIGH_W:0] upper_sum_temp;
   logic [PARTIAL_SUM_HIGH_W-1:0] mantissa_sum_upper;
+  logic [ PARTIAL_SUM_LOW_W-1:0] mantissa_sum_lower;
 
   always_comb begin
-    mult_result = norm_mant_a_i * norm_mant_b_i;
+    foreach (partial_products[i]) begin
+      logic [MANTISSA_W-1:0] partial_product;
+      partial_product     = norm_mant_a_i & {MANTISSA_W{norm_mant_b_i[i]}};
+      partial_products[i] = {{(MANTISSA_W + 1) {1'b0}}, partial_product} << i;
+    end
 
-    mantissa_sum_lower = PARTIAL_SUM_LOW_W'(mult_result) + PARTIAL_SUM_LOW_W'(csa_c_i);
+    for (int i = 0; i < NUM_PARTIAL_PRODUCTS; i++) begin
+      csa_summands[i] = partial_products[i];
+    end
+    csa_summands[NUM_WALLACE_INPUTS-1] = {1'b0, csa_c_i};
+  end
 
+  wallace_tree_recursive #(
+      .DATA_W  (LOW_SUM_W),
+      .NUM_ROWS(NUM_WALLACE_INPUTS)
+  ) wallace_tree_inst (
+      .partial_sums(csa_summands),
+      .sum         (csa_tree_sum),
+      .carry       (csa_tree_carry)
+  );
+
+  always_comb begin
+    mantissa_sum_lower = csa_tree_sum + csa_tree_carry;
     upper_sum_temp = {c_upper_slice_i[PARTIAL_SUM_HIGH_W-1], c_upper_slice_i}
                    + (PARTIAL_SUM_HIGH_W + 1)'(mantissa_sum_lower[PARTIAL_SUM_LOW_W-1 : PARTIAL_SUM_LOW_W-2]);
 
     mantissa_sum_upper = upper_sum_temp[PARTIAL_SUM_HIGH_W:1];
-
     mantissa_sum_raw_o = {
       mantissa_sum_upper, upper_sum_temp[0], mantissa_sum_lower[PARTIAL_SUM_LOW_W-3:0]
     };
+
   end
 
 endmodule
